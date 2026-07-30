b3a03c42cb1e46196c5e431a37c0e65cd2b7fa0c
Abdullah Khataan <abdoallah.khataan@gmail.com>
2026-07-30 22:36:12 +0200

    EmbVars.h: add brackets to preserve orders of opperation

--- include/EmbVars.h
+++ include/EmbVars.h
@@ -37,23 +37,23 @@ _Static_assert(sizeof(f64) == 8, "sizeof(f64) != 8");
 #define LOW 0
 
 // A
-#define DDRA *(volatile u8*) 0x3A
-#define PORTA *(volatile u8*) 0x3B
-#define PINA *(volatile u8*) 0x39
+#define DDRA (*(volatile u8*) 0x3A)
+#define PORTA (*(volatile u8*) 0x3B)
+#define PINA (*(volatile u8*) 0x39)
 
 // B
-#define DDRB *(volatile u8*) 0x37
-#define PORTB *(volatile u8*) 0x38
-#define PINB *(volatile u8*) 0x36
+#define DDRB (*(volatile u8*) 0x37)
+#define PORTB (*(volatile u8*) 0x38)
+#define PINB (*(volatile u8*) 0x36)
 
 //C
-#define DDRC *(volatile u8*) 0x34
-#define PORTC *(volatile u8*) 0x35
-#define PINC *(volatile u8*) 0x33
+#define DDRC (*(volatile u8*) 0x34)
+#define PORTC (*(volatile u8*) 0x35)
+#define PINC (*(volatile u8*) 0x33)
 
 //D
-#define DDRD *(volatile u8*) 0x31
-#define PORTD *(volatile u8*) 0x32
-#define PIND *(volatile u8*) 0x30
+#define DDRD (*(volatile u8*) 0x31)
+#define PORTD (*(volatile u8*) 0x32)
+#define PIND (*(volatile u8*) 0x30)
 
 #endif
