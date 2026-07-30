149223c8d2cf058c51b5739e797da72036a14de9
Ahmad Khalifa <ahmadkhalifa570@gmail.com>
2026-07-30 23:06:48 +0300

    Revert "Merge branch 'main' of https://github.com/Budie200/NTI-Smart-Home-Project"
    This reverts commit 43bdde126eaff709617369aa29dc2abec8d80e33, reversing
    changes made to fa505b843077820fa155dd11ea9dc8cd31b2e698.
    
    It was added by accident.

--- include/STD_TYPES.h
+++
@@ -1,7 +0,0 @@
-#define STD_TYPES_H
-typedef unsigned char u8;
-typedef unsigned short int u16;
-typedef unsigned long int u32;
-typedef signed char s8;
-typedef signed short int s16;
-typedef signed long int s32;
