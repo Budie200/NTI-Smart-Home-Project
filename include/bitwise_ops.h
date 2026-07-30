61638394400ac34be0e2dfd9a5c59787abe8cd8d
Ahmad Khalifa <ahmadkhalifa570@gmail.com>
2026-07-31 01:36:20 +0300

    bitwise_ops: define a bit() macro
    Usefull if we want to set multiple flags in a register.

--- include/bitwise_ops.h
+++ include/bitwise_ops.h
@@ -1,6 +1,8 @@
 #ifndef BITWISE_OPS
 #define BITWISE_OPS
 
+#define bit(pos)	(1 << (pos))
+
 #define set_bit(val, pos) ((val) |= (1 << pos))
 #define clr_bit(val, pos) ((val) &= ~(1 << pos))
 #define toggle_bit(val, pos) ((val) ^= (1 << pos))
