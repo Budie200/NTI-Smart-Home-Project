104fc4a52fb2530195038c0432a61381aa588ebd
Ahmad Khalifa <ahmadkhalifa570@gmail.com>
2026-08-01 18:15:34 +0300

    EmbVars: define interrupt bit

--- include/EmbVars.h
+++ include/EmbVars.h
@@ -69,5 +69,6 @@ _Static_assert(sizeof(f64) == 8, "sizeof(f64) != 8");
 #define SPH (*(volatile u8*) 0x5e)
 #define SP (*(volatile u8**) 0x5d)
 #define SREG (*(volatile u8*) 0x5f)
+#define SREG_INTREN	7
 
 #endif
