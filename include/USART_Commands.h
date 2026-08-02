3bd29c3e91464111a19b1178b8bcfbdabf8f60b2
Ahmad Khalifa <ahmadkhalifa570@gmail.com>
2026-08-02 03:53:50 +0300

    USART_Commands.h: better

--- include/USART_Commands.h
+++ include/USART_Commands.h
@@ -3,12 +3,11 @@
 
 /* Support commands for AVR1 and AVR2. */
 
-enum USART_Commands {
-	USART_CMD_LIGHT_ON,
-	USART_CMD_LIGHT1_OFF,
-	USART_CMD_FAN_ON,
-	USART_CMD_MODE_AUTO,
-	USART_CMD_MODE_MANUAL
-};
+#define USART_CMD_LIGHT1_ON	0
+#define USART_CMD_LIGHT1_OFF	1
+#define USART_CMD_FAN_ON	2
+#define USART_CMD_FAN_OFF	3
+#define USART_CMD_FAN_DIR_L	4
+#define USART_CMD_FAN_DIR_R	5
 
 #endif
